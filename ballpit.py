cba5cfaf25dfdd66f264c0e2ecef61572b138bd9
Hannah <hmd1@aber.ac.uk>
2018-04-17 11:53:05 +0100

    added sound play via sounddevice

--- ballpit.py
+++ ballpit.py
@@ -9,37 +9,53 @@ import video
 import sys
 import numpy as np
 from Hotspot import Hotspot
+from os import listdir
+from os.path import isfile, join
+import sounddevice as sd
+import soundfile as sf
 
 hist=5
 wid=5
 no_sounds=8
 no=0
+dirname='/home/hannah/coding/ballpit/sounds/'
 
 hotspots=[]
-for i in range (no_sounds):
-    x=Hotspot(hist,wid) 
-    hotspots.append(x)
 
 # mouse callback function
 def add_hotspot(event,x,y,flags,param):
     global no
     if event == cv2.EVENT_LBUTTONUP:
         if (no<no_sounds):
-		print(x,y,no)
-		hotspots[no].update_location(x,y)
-                no+=1
+            print(x,y,no)
+            hotspots[no].update_location(x,y)
+            no+=1
 
 if __name__ == '__main__':
-    print __doc__
+    print(__doc__)
 
 # if we have a filename let's have a go at opening that...
     try: fn = sys.argv[1]
     except: fn = 0
-
-    events = [i for i in dir(cv2) if 'EVENT' in i]
-    print events
-    def nothing(*arg):
+    def nothing(*argv):
         pass
+
+
+    
+    soundfiles = [join(dirname,f) for f in listdir(dirname) if isfile(join(dirname,f))] 
+    
+    no_sounds=len(soundfiles)
+    print(no_sounds)
+    print(soundfiles)
+    sounds = []
+    fs = []
+    for i in range (no_sounds):
+         x=Hotspot(hist,wid) 
+         hotspots.append(x)
+         s,f =sf.read(soundfiles[i])
+         sounds.append(s)
+         fs.append(f)
+         sd.play(sounds[i], fs[i])
 
 # gizza window with a trackbar on it
     cv2.namedWindow('bgmodel')
@@ -52,7 +68,8 @@ if __name__ == '__main__':
 # gizza videocapture object
     cap = video.create_capture(fn)
 
-    flag, img = cap.read()
+    flag, im = cap.read()
+    img=cv2.flip(im,1)
     movingaverage=np.float32(img)
     while True:
 
@@ -85,16 +102,14 @@ if __name__ == '__main__':
         ret,th1 = cv2.threshold(grey_difference_img,difference_thresh,255,cv2.THRESH_BINARY)
         fgimg=cv2.merge((th1,th1,th1))
         intimg=cv2.integral(th1)
-	for h in hotspots:
-	    cv2.rectangle(fgimg,h.tl,h.br,(255,0,0),3)
-            if (h.is_moving(intimg)):
-	        cv2.rectangle(fgimg,h.tl,h.br,(0,255,0),3)
+        i=0
+        for h in hotspots:
+           cv2.rectangle(fgimg,h.tl,h.br,(255,0,0),3)
+           if (h.is_moving(intimg)):
+               cv2.rectangle(fgimg,h.tl,h.br,(0,255,0),3)
+               sd.play(sounds[i])
+           i=i+1
         cv2.imshow('foregound',fgimg)
-#uncommment the next few lines if you want to save the output
-        #fn="out/bgmovingav_big"+str(n).rjust(4,'0')+".png" 
-        #cv2.imwrite(fn,th1);
-        #fn="out/bgmovingav_bg_big"+str(n).rjust(4,'0')+".png" 
-        #cv2.imwrite(fn,res);
         n+=1
  
 
